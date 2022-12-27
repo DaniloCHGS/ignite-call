e47f1727f8f382281700969cf83ac88e1cc39bc9
DaniloCHGSDev <danilobm500@gmail.com>
2022-12-27 16:28:08 -0300

    Validação de formulário

--- src/pages/home/components/ClainUsernameForm/index.tsx
+++ src/pages/home/components/ClainUsernameForm/index.tsx
@@ -1,14 +1,36 @@
 import { Button, Text, TextInput } from "@ignite-ui/react";
 import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Form } from "./style";
 
-export function ClainUsernameForm(){
+const ClainUsernameFormSchema = z.object({
+    username: z.string()
+    .min(3, {message: ''})
+    .regex(/^([a-z\\-]+)$/i)
+    .transform(value => value.toLowerCase())
+})
+
+type ClainUsernameFormData = z.infer<typeof ClainUsernameFormSchema>
+
+export function ClainUsernameForm() {
+
+    const { register, handleSubmit } = useForm<ClainUsernameFormData>({
+        resolver: zodResolver(ClainUsernameFormSchema)
+    })
+
+    async function handleClainUsername(data: ClainUsernameFormData) {
+        console.log(data)
+    }
+
     return (
-        <Form as="form">
+        <Form as="form" onSubmit={handleSubmit(handleClainUsername)}>
             <TextInput
                 size="sm"
                 prefix="ingite.com/"
                 placeholder="seu-usuário"
+                {...register("username")}
             />
             <Button
                 size="sm"
